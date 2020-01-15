560d50be530d2ec637dfb220726c0d6a81b0cf93
Lars Hellemo <hellemo@gmail.com>
2020-01-15 22:49:59 +0100

    :white_check_mark: Added tests for @sparsevariable.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using Test
 using SparseHelper
+using JuMP
 
 ts = [(i=:A,j=2,k=5),(i=:B,j=5,k=4),(i=:C,j=3,k=5)]
 
@@ -16,4 +17,15 @@ end
     @test length(select(ts,(:*,:*,5))) == 2
     @test length(select(ts,(:*,*,5))) == 0
     @test select(ts,(:B,:*,:*))[1][1] == :B
+end
+
+@testset "Macros" begin
+    m = Model()
+    @sparsevariable(m,x[i,j,k] for (i,j,k) in ts)
+    @test typeof(m[:x][:C,3,5]) == VariableRef
+    @test name(m[:x][:C,3,5]) == "x[C, 3, 5]"
+    @sparsevariable(m,y[i,j] for (i,j) = [(1,2),(100,200)])
+    @test typeof(m[:y][100,200]) == VariableRef
+    @sparsevariable(m,z[u] for u = [1,10])
+    @test typeof(m[:z][1]) == VariableRef
 end
